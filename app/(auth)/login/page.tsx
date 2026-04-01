8fb30a552b9df7b282dbf56a5ae3b5f16b51e425
mohamedaasif <mohamedaasif.md@gmail.com>
2026-04-01 17:36:08 +0530

    add login page handling

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,58 @@
 "use client";
 
 import AuthBanner from "@/app/_components/AuthBanner";
+import { loginUser } from "@/app/_feature/auth/authThunk";
+import { useAppDispatch, useAppSelector } from "@/app/_feature/hooks";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Eye, EyeOff } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Loader2 } from "lucide-react";
+
+interface FormInput {
+  email: string;
+  password: string;
+}
 
 export default function Login() {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const { user, isAuthenticated, loading, error } = useAppSelector(
+    (store) => store.auth,
+  );
+
+  const schema = z.object({
+    email: z.email(),
+    password: z.string(),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInput>({
+    resolver: zodResolver(schema),
+  });
+
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const handleLogin = async (data: FormInput) => {
+    const { email, password } = data;
+    if (!email || !password) return;
+    const result = await dispatch(
+      loginUser({ emailId: email, password: password }),
+    )
+      .then((res) => {
+        router.replace("/dashboard");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="min-h-screen flex">
       <AuthBanner
@@ -19,14 +68,24 @@ export default function Login() {
           </h2>
           <p className="text-gray-500 mb-6">Login to your account</p>
 
-          <form className="space-y-4">
+          <form
+            className="space-y-4"
+            onSubmit={handleSubmit(handleLogin)}
+            noValidate
+          >
             <div>
               <label className="text-sm text-gray-600">Email</label>
               <input
                 type="email"
-                placeholder="user@example.com"
+                {...register("email", { required: "Email is required" })}
+                placeholder="Enter Email"
                 className="w-full mt-1 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
               />
+              {errors.email && (
+                <p className="text-red-600 text-sm pt-1 pl-1">
+                  {errors.email.message}
+                </p>
+              )}
             </div>
 
             <div>
@@ -36,14 +95,38 @@ export default function Login() {
                   Forgot password?
                 </span>
               </div>
-              <input
-                type="password"
-                className="w-full mt-1 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              />
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  {...register("password", {
+                    required: "Password is required",
+                  })}
+                  placeholder="Enter Password"
+                  className="w-full mt-1 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
+                >
+                  {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                </button>
+              </div>
+              {errors.password && (
+                <p className="text-red-600 text-sm pt-1 pl-1">
+                  {errors?.password?.message}
+                </p>
+              )}
             </div>
-
-            <button className="w-full bg-indigo-600 cursor-pointer text-white py-2 rounded-lg hover:bg-indigo-700 transition">
-              Log In
+            {error?.message && (
+              <p className="text-red-600 text-sm pl-1">{error?.message}</p>
+            )}
+            <button
+              type="submit"
+              disabled={loading}
+              className="w-full flex items-center justify-center bg-indigo-600 cursor-pointer text-white py-2 rounded-lg hover:bg-indigo-700 transition"
+            >
+              {loading ? <Loader2 className="animate-spin" /> : "Log In"}
             </button>
 
             <p className="text-sm text-center text-gray-500 mt-6">
