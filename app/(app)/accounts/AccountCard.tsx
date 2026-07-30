afae3b2a4cd15ed46e4320d731cc913ee1b0cad0
Aasif <aasif@Aasifs-MacBook-Air.local>
2026-07-30 22:05:50 +0530

    add account page ui

--- app/(app)/accounts/AccountCard.tsx
+++ app/(app)/accounts/AccountCard.tsx
@@ -1,18 +1,62 @@
 "use client";
 
-import { Landmark } from "lucide-react";
+import {
+  Banknote,
+  ChartNoAxesCombined,
+  CreditCard,
+  Landmark,
+  QrCode,
+} from "lucide-react";
 import styles from "./AccountCard.module.css";
 import { formatCurrency } from "@/lib/utils";
 import { Account } from "@/app/_types/accounts";
 
 const AccountCard = ({ data }: { data: Account }) => {
+  const stripeClass = {
+    Savings: styles.sav,
+    Credit_Card: styles.neg,
+    Cash: styles.cash,
+    Investment: styles.invest,
+    Current: styles.current,
+    Wallet: styles.wallet,
+  };
+  const iconClass = {
+    Savings: styles["sav-ico"],
+    Credit_Card: styles["neg-ico"],
+    Cash: styles["cash-ico"],
+    Investment: styles["invest-ico"],
+    Current: styles["current-ico"],
+    Wallet: styles["wallet-ico"],
+  };
+  const iconBgClass = {
+    Savings: styles["sav-bg"],
+    Credit_Card: styles["neg-bg"],
+    Cash: styles["cash-bg"],
+    Investment: styles["invest-bg"],
+    Current: styles["current-bg"],
+    Wallet: styles["wallet-bg"],
+  };
+
+  const icons = {
+    Savings: Landmark,
+    Credit_Card: CreditCard,
+    Cash: Banknote,
+    Investment: ChartNoAxesCombined,
+    Current: Landmark,
+    Wallet: QrCode,
+  };
+
+  const Icon = icons[data.type] ?? Landmark;
+
   return (
     <div className={styles["stat-card"]}>
-      <div className={styles["stat-stripe"] + " " + styles["t-bal"]}></div>
+      <div
+        className={`${styles["stat-stripe"]} ${stripeClass[data?.type]}`}
+      ></div>
       <div>
         <div className="flex justify-between mb-2">
-          <div className={styles["stat-ico"] + " " + styles["t-bal-ico"]}>
-            <Landmark className="text-[var(--color-pos-text)]" size={16} />
+          <div className={`${styles["stat-ico"]} ${iconBgClass[data.type]}`}>
+            <Icon className={iconClass[data.type]} size={16} />
           </div>
           <div className="flex items-center gap-3">
             <span className={styles["acc-typ"]}>
